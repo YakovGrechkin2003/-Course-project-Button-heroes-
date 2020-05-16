3f03f3d2f6203a36cffdc0ef81c7a4b305489b0b
YakovGrechkin2003 <58858243+YakovGrechkin2003@users.noreply.github.com>
2020-05-16 16:11:34 +0300

    button.hpp

---
+++ button.hpp
@@ -0,0 +1,51 @@
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
+
+#include <QObject>
+#include <QGraphicsItem>
+#include <QPointF>
+#include <QPixmap>
+
+class button : public QObject, public QGraphicsItem
+{
+  Q_OBJECT
+public:
+  button();
+
+  QRectF boundingRect() const override;
+  void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+
+  void mousePressEvent(QGraphicsSceneMouseEvent *)  override;
+  void mouseMoveEvent(QGraphicsSceneMouseEvent *)   override;
+  void mouseReleaseEvent(QGraphicsSceneMouseEvent *)override;
+
+  void hoverEnterEvent(QGraphicsSceneHoverEvent*)   override;
+  void hoverLeaveEvent(QGraphicsSceneHoverEvent*)   override;
+
+  void enableMouseMoving();
+  void disableMouseMoving();
+
+  void setGeometry(int, int);
+  void setImage(QString);
+
+signals:
+  void btnMouseReleaseEvent(Qt::MouseButton);
+
+private:
+  int mWidth{0};
+  int mHeigth{0};
+
+  bool mIsMovable{true};
+  bool mIsMoving{false};
+  bool mHover{false};
+
+public:
+  int coordX{0};
+  int coordY{0};
+
+  QPixmap mPixMap;
+
+  QPointF mStartMovePos;
+};
+
+#endif // BUTTON_HPP
